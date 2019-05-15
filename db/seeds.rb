d32680a4f77154f864672fb586012dbc7804882b
Charline Mons <charline.mons@gmail.com>
2019-05-15 16:09:57 +0200

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurant1 = Restaurant.create!(name: "Christian Pizza", address: "Lyon", category: "italian" )
+restaurant2 = Restaurant.create!(name: "Sushi St Denis", address: "toulouse", category: "chinese" )
+restaurant3 = Restaurant.create!(name: "Crepe Delice", address: "bordeaux", category: "belgian" )
+restaurant4 = Restaurant.create!(name: "pizza hut", address: "lyon", category: "italian" )
+restaurant5 = Restaurant.create!(name: "chez mama", address: "lyon", category: "italian" )
